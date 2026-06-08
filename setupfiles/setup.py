6e37a020ef216fb6819176e936b00ed93007f309
veritus-git <veritus.github@gmail.com>
2026-06-08 23:40:22 +0200

    visual adjustment for installer

--- setupfiles/setup.py
+++ setupfiles/setup.py
@@ -1501,14 +1501,14 @@ def option_install_update(force_main=False, preset_path=None, title="── Stan
         console.print(Text(f"{PAD}   Path : {install_dir}", style="green"), no_wrap=True)
         console.print(Text(f"{PAD}   Log  : {log_file}", style="green"), no_wrap=True)
         console.print()
-        console.print(Text(f"{PAD}IMPORTANT FOR DAVINCI RESOLVE:", style="bold yellow"), no_wrap=True)
+        console.print(Text(f"{PAD}   IMPORTANT FOR DAVINCI RESOLVE:", style="bold yellow"), no_wrap=True)
         if py_auto_installed:
-            console.print(Text(f"{PAD}1. Python was installed in the background.", style="yellow"), no_wrap=True)
-            console.print(Text(f"{PAD}2. You MUST restart your computer for Resolve to detect it.", style="bold yellow"), no_wrap=True)
-            console.print(Text(f"{PAD}3. Find the script in: Workspace -> Scripts -> BadWords", style="yellow"), no_wrap=True)
+            console.print(Text(f"{PAD}   1. Python was installed in the background.", style="yellow"), no_wrap=True)
+            console.print(Text(f"{PAD}   2. You MUST restart your computer for Resolve to detect it.", style="bold yellow"), no_wrap=True)
+            console.print(Text(f"{PAD}   3. Find the script in: Workspace -> Scripts -> BadWords", style="yellow"), no_wrap=True)
         else:
-            console.print(Text(f"{PAD}1. You MUST restart DaVinci Resolve.", style="yellow"), no_wrap=True)
-            console.print(Text(f"{PAD}2. Find the script in: Workspace -> Scripts -> BadWords", style="yellow"), no_wrap=True)
+            console.print(Text(f"{PAD}   1. A restart of DaVinci Resolve might be required.", style="yellow"), no_wrap=True)
+            console.print(Text(f"{PAD}   2. Find the script in: Workspace -> Scripts -> BadWords", style="yellow"), no_wrap=True)
         console.print()
 
     finally:
