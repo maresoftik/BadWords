4c41e7d74355b7a74b24ac9d2429a10fcbb66f33
veritus-git <veritus.github@gmail.com>
2026-06-24 22:59:39 +0200

    changed webhook for Contact tab - to stop spam

--- src/config.py
+++ src/config.py
@@ -20,7 +20,7 @@ import platform
 # ==========================================
 APP_NAME = "BadWords"
 VERSION = "3.1.0"
-SUPPORT_WEBHOOK_URL = "https://discord.com/api/webhooks/1518008475979087913/He5i09a5Q5LSuRp6r2cCtKio4OYMcPGUWTmMdOo8RfyLrrYSYGl6WbV9DHCq5PbhGzSx"
+SUPPORT_WEBHOOK_URL = "http://frog02.mikr.us:41385/"
 POSTHOG_API_KEY = "phc_NRFaTKhPJJE8cBa3o9gXYo2mlktR5qup5tn7PdxuRsr"
 POSTHOG_HOST = "https://eu.i.posthog.com"
 
